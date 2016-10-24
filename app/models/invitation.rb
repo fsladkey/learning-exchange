34b3185203a2d799bf6d29b4dc022a2f5b8e4e60
Fred Sladkey <fsladkey@gmail.com>
2016-10-24 17:03:56 -0400

    Add validations and associations to invitations

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: invitations
+#
+#  id         :integer          not null, primary key
+#  event_id   :integer
+#  inviter_id :integer
+#  invitee_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Invitation < ApplicationRecord
+  validates :event, :inviter, :invitee, presence: true
+
+  belongs_to :event
+  belongs_to :inviter, class_name: :User
+  belongs_to :invitee, class_name: :User
 end
