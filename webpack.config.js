04834f6e096732726b38c0a8a02a9ca7bac26078
Fred Sladkey <fsladkey@gmail.com>
2016-10-24 11:49:23 -0400

    Create frontend config files

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var plugins = [
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV':
+      JSON.stringify(process.env.NODE_ENV || 'development')
+  })
+]
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    })
+  )
+}
+
+module.exports = {
+  context: __dirname,
+  entry: { filename: path.join(__dirname, 'frontend', 'entry.js') },
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  plugins: plugins,
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.js'],
+    root: path.resolve('./node_modules')
+  }
+};
