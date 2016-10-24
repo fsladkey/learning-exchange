fe23341985a0e14ff986a89430a8cc2b560b4e22
Fred Sladkey <fsladkey@gmail.com>
2016-10-24 16:55:37 -0400

    Add group model

---
+++ app/models/group.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: groups
+#
+#  id          :integer          not null, primary key
+#  name        :string           not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Group < ApplicationRecord
+  validates :name, presence: true
+
+  has_many :events
+  has_many :memberships
+  has_many :members, through: :memberships, source: :member
+end
---
+++ spec/factories/groups.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: groups
+#
+#  id          :integer          not null, primary key
+#  name        :string           not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+FactoryGirl.define do
+  factory :group do
+    
+  end
+end
