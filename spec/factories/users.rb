225a69df0c1c0fca24108c9149338048e1262a6a
Fred Sladkey <fsladkey@gmail.com>
2016-10-24 14:49:14 -0400

    set up factory girl

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,36 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  username               :string           not null
+#  firstname              :string
+#  lastname               :string
+#  zipcode                :string
+#  latitude               :float            not null
+#  longitude              :float            not null
+#
+
 FactoryGirl.define do
   factory :user do
-    
+    firstname { Faker::Name.first_name }
+    lastname  { Faker::Name.last_name }
+    username  { Faker::Internet.user_name }
+    email  { Faker::Internet.email }
+    zipcode  { Faker::Address.zip_code }
+    latitude  { Faker::Address.latitude }
+    longitude  { Faker::Address.longitude }
   end
 end
