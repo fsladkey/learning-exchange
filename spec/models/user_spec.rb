eab757106a97b9f0e5068a09a31796e2a04af2ff
Fred Sladkey <fsladkey@gmail.com>
2016-10-24 14:04:30 -0400

    Update user spec to test devise columns

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,26 @@
-describe User do
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { should have_db_column(:email) }
   it { should have_db_column(:username) }
   it { should have_db_column(:firstname) }
   it { should have_db_column(:lastname) }
   it { should have_db_column(:session_token) }
-  it { should have_db_column(:password_digest) }
+  it { should have_db_column(:encrypted_password) }
+  it { should have_db_column(:reset_password_token) }
+  it { should have_db_column(:reset_password_sent_at) }
+  it { should have_db_column(:remember_created_at) }
+  it { should have_db_column(:sign_in_count) }
+  it { should have_db_column(:current_sign_in_at) }
+  it { should have_db_column(:last_sign_in_at) }
+  it { should have_db_column(:current_sign_in_ip) }
+  it { should have_db_column(:last_sign_in_ip) }
   it { should have_db_column(:zipcode) }
   it { should have_db_column(:latitude) }
   it { should have_db_column(:longitude) }
+  it { should have_db_column(:created_at) }
+  it { should have_db_column(:updated_at) }
 
-  it { should have_secure_password }
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:session_token) }
