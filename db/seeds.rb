27204ed6795517adb758f8ef723bd4e593dff7d2
Fred Sladkey <fsladkey@gmail.com>
2016-10-25 17:39:18 -0400

    Fix bad seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 #Users
+puts "creating users..."
 User.destroy_all
 FactoryGirl.create(:user, username: "fsladkey", email: "fsladkey@gmail.com")
 50.times do
@@ -6,13 +7,17 @@ FactoryGirl.create(:user, username: "fsladkey", email: "fsladkey@gmail.com")
 end
 
 #Groups
-User.destroy_all
+puts "creating groups..."
+Group.destroy_all
 10.times do
   FactoryGirl.create(:group)
 end
 
-#Groups
-User.destroy_all
+#Events
+puts "creating events..."
+Event.destroy_all
+group_ids = Group.pluck(:id)
+user_ids = User.pluck(:id)
 20.times do
-  FactoryGirl.create(:event)
+  FactoryGirl.create(:event, group_id: group_ids.sample, creator_id: user_ids.sample)
 end
