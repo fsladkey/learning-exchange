e29b896eff0559761e387ab26cf8920135525c51
Fred Sladkey <fsladkey@gmail.com>
2016-11-25 20:13:56 -0500

    Add rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,30 @@
 Rails.application.routes.draw do
+  root "static_pages#root"
+  get "welcome", to: "static_pages#welcome"
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    resources :search, only: [:index]
+    resources :messages, only: [:update, :delete]
+    resources :groups, expect: [:new, :edit]
+    resources :events, expect: [:new, :edit]
+    resources :notifications, only: [:update]
+
+    resource :users do
+      resources :messages, only: [:create]
+    end
+
+    resource :groups do
+      resources :messages, only: [:create]
+    end
+
+    resource :events do
+      resources :comments, only: [:create]
+    end
+
+    get "session/events", to: "events#current_user_events"
+    get "session/groups", to: "groups#current_user_groups"
+  end
+
 end
