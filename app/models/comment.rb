f0a667d714bf88fa7c95e23dc6005c889be5b725
Fred Sladkey <fsladkey@gmail.com>
2016-12-04 00:09:04 -0500

    Add comment model and spec

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id               :integer          not null, primary key
+#  body             :text
+#  author_id        :integer
+#  deleted          :boolean          default(FALSE)
+#  commentable_type :string
+#  commentable_id   :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class Comment < ApplicationRecord
   validates :body, :author, presence: true
 
