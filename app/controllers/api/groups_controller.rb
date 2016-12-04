814e4eb2c32d76d5ec35520d834832c5029d10a1
Fred Sladkey <fsladkey@gmail.com>
2016-12-04 00:07:41 -0500

    Add groups controller and spec

---
+++ app/controllers/api/groups_controller.rb
@@ -0,0 +1,51 @@
+# ### Groups Controller
+# `GET /api/groups/`
+# `GET /api/session/groups/`
+# `POST /api/groups/`
+# `PATCH /api/groups/`
+# `DELETE /api/groups/`
+
+class Api::GroupsController < Api::ApiController
+
+  def index
+    @groups = Group.all
+  end
+
+  def current_user_groups
+    @groups = current_user.groups
+    render :index
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      render :show
+    else
+      render json: group.errors, status: 422
+    end
+  end
+
+  def update
+    if group.update(group_params)
+      render :show
+    else
+      render json: group.errors, status: 422
+    end
+  end
+
+  def destroy
+    group.destroy
+    render :show
+  end
+
+  private
+
+  def group
+    @group ||= Group.find(params[:id])
+  end
+
+  def group_params
+    params.require(:group).permit(:name, :desciption)
+  end
+
+end
---
+++ spec/controllers/api/groups_controller_spec.rb
@@ -0,0 +1,92 @@
+# ### Groups Controller
+# `GET /api/groups/`
+# `GET /api/session/groups/`
+# `POST /api/groups/`
+# `PATCH /api/groups/`
+# `DELETE /api/groups/`
+
+require 'rails_helper'
+
+RSpec.describe Api::GroupsController, type: :controller do
+  describe "while not logged in" do
+    before do
+      allow(controller).to receive(:user_signed_in?).and_return(false)
+    end
+
+    it "rejects unauthorized requests" do
+      get :index
+      expect(controller).to respond_with(401)
+    end
+
+  end
+
+  describe "while logged in" do
+    before do
+      allow(controller).to receive(:current_user) { User.new }
+    end
+
+    describe "GET index" do
+      it "renders the index template" do
+        get :index
+        expect(controller).to render_template(:index)
+      end
+    end
+
+    describe "GET show" do
+      it "renders the show template" do
+        group = create(:group)
+        get :show, params: { id: group.id }
+        expect(controller).to render_template(:show)
+      end
+    end
+
+    describe "GET current_user_groups" do
+      it "renders the current_user_groups template" do
+        get :current_user_groups
+        expect(controller).to render_template(:index)
+      end
+    end
+
+    describe "POST create" do
+      it "creates a new group with valid params" do
+        post :create, params: {
+          group: { name: "Group name", description: "Description" }
+        }
+        expect(controller).to render_template(:show)
+        expect(Group.exists?(name: "Group name")).to be(true)
+      end
+
+      it "rejects invalid params" do
+        post :create, params: { group: { description: "Description" } }
+        expect(controller).to respond_with(422)
+        expect(Group.exists?(description: "Description")).to be(false)
+      end
+
+    end
+
+    describe "PATCH update" do
+      it "updates a group with valid params" do
+        group = create(:group, name: "Original")
+        patch :update, params: { id: group.id, group: { name: "Updated" } }
+        expect(controller).to render_template(:show)
+        expect(Group.find(group.id).name).to eq("Updated")
+      end
+
+      it "renders 422 with invalid params" do
+        group = create(:group)
+        patch :update, params: { id: group.id, group: { name: "" } }
+        expect(controller).to respond_with(422)
+      end
+    end
+
+    describe "DELETE destroy" do
+      it "destroys the group if it exists" do
+        group = create(:group)
+        delete :destroy, params: { id: group.id }
+        expect(controller).to render_template(:show)
+        expect(Group.exists?(id: group.id)).to be(false)
+      end
+    end
+
+  end
+end
