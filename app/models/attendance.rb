a159fd455564ea9be3431779679266acc86546f1
Fred Sladkey <fsladkey@gmail.com>
2016-12-04 00:08:04 -0500

    Add attendance model and spec

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: attendances
+#
+#  id         :integer          not null, primary key
+#  event_id   :integer          not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Attendance < ApplicationRecord
   validates :user, :event, presence: :true
 
--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: attendances
+#
+#  id         :integer          not null, primary key
+#  event_id   :integer          not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Attendance, type: :model do
