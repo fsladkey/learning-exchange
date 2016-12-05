0c978b734b60e0038650a7f2a57ff7bebcbfd2bc
Fred Sladkey <fsladkey@gmail.com>
2016-12-04 22:25:04 -0500

    Add yarn

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'latest']
         }
       }
     ]
