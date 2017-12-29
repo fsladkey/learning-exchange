aa4781569f6272e2be9537546ab960b6d5fd0432
Fred Sladkey <fsladkey@gmail.com>
2017-12-28 23:15:11 -0600

    Fix webpack dev server

--- webpack.hot.config.js
+++ webpack.hot.config.js
@@ -2,12 +2,12 @@ var path = require('path');
 
 module.exports = Object.assign(require("./webpack.config"), {
   entry: [
-    'webpack-dev-server/client?http://0.0.0.0:8081',
+    'webpack-dev-server/client?http://0.0.0.0:8080',
     'webpack/hot/only-dev-server',
     path.join(__dirname, 'frontend', 'entry.js')
   ],
   output: {
     filename: 'bundle.js',
-    publicPath: 'http://localhost:8081/javascripts'
+    publicPath: 'http://localhost:8080/javascripts'
   },
 });
