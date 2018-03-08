080a81e3a22114a96ade33503526d135c83da6d7
Fred Sladkey <fsladkey@gmail.com>
2018-03-07 21:34:59 -0500

    Fix attending user list

--- frontend/components/events/event_members.js
+++ frontend/components/events/event_members.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { connect } from "react-redux"
 import EventMemberList from "./event_member_list"
+import { attendees } from "../../reducers/selectors"
 
-export function EventMembers({ event }) {
+export function EventMembers({ attendees }) {
   return (
     <section className="event-members">
       <h1>Event Attendees</h1>
-      <EventMemberList members={event.attending_users} />
+      <EventMemberList members={attendees} />
     </section>
   )
 }
 
-const mapStateToProps = ({ events }, { params }) => ({
-  event: events.get(params.id)
+const mapStateToProps = (state, { params }) => ({
+  attendees: attendees(state, params.id)
 })
 
 export default connect(mapStateToProps)(EventMembers)
