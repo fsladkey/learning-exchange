9de147477f9b9635e9bc3495d1c98c34e0a9573f
Fred Sladkey <fsladkey@gmail.com>
2018-07-08 20:26:03 -0400

    Add timestamp to messages

--- frontend/components/shared/message_list.js
+++ frontend/components/shared/message_list.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react'
-import { moment } from 'moment'
+import moment from 'moment'
 import UserPhoto from '../shared/user_photo'
 
 const MOMENT_FORMAT = "dddd, MMMM Do YYYY, h:mm:ss a"
 
+
 function Message({ message, currentUser }) {
-  if (message.sender_id === currentUser.id) {
-    return (
-      <li className="message-item">
-        <UserPhoto user={ message.sender } className="thumb" />
-        <p className="current-user message">{ message.body }</p>
-        <p>{moment(message.created_at).format(MOMENT_FORMAT) }</p>
-      </li>
-    )
-  }
+  const isCurrentUserMessage = message.sender_id === currentUser.id;
   return (
-    <li className="message-item">
-      <p className="other-user message">{ message.body }</p>
-      <UserPhoto user={ message.sender } className="thumb" />
-      <p>{moment(message.created_at).format(MOMENT_FORMAT)}</p>
+    <li className={`message-item ${isCurrentUserMessage ? "left" : "right"}`}>
+      <div className="message-row">
+        <UserPhoto user={ message.sender } className="thumb" />
+        <p className="message">
+          { message.body }
+        </p>
+      </div>
+      <p className="message-timestamp">{moment(message.created_at).format(MOMENT_FORMAT) }</p>
     </li>
   )
 }
@@ -38,18 +35,18 @@ export default class MessageList extends Component {
   }
 
   componentWillReceiveProps() {
-    setTimeout(() => this.scrollToBottom(1), 10)
+    setTimeout(() => this.scrollToBottom(true), 10)
   }
 
   render() {
     const { messages, currentUser } = this.props
     return (
       <ul ref={ node => this.node = node } className="message-list">
-      {
-        messages.map(message =>
-          <Message key={ message.id } message={ message } currentUser={ currentUser }/>
-        )
-      }
+        {
+          messages.map(message =>
+            <Message key={ message.id } message={ message } currentUser={ currentUser }/>
+          )
+        }
       </ul>
     )
   }
